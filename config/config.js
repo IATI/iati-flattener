3bc38a5dbb1c1bbababd340d490944243ddd6b04
nosvalds <niko@devinit.org>
2021-10-26 10:02:27 +0100

    fix version endpoint

--- config/config.js
+++ config/config.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
+const { version } = require('../package.json');
 
 module.exports = {
-    APP_NAME: 'IATI App Name Here',
-    VERSION: process.env.npm_package_version,
+    APP_NAME: 'IATI Flattener and Schema Converter',
+    VERSION: version,
     NODE_ENV: process.env.NODE_ENV,
     APPINSIGHTS_INSTRUMENTATIONKEY: process.env.APPINSIGHTS_INSTRUMENTATIONKEY,
     NS_PER_SEC: 1e9,
